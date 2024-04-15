2ad086e97e7c01c7d691364b8322c3c3c0f36d63
Tikhon <tihonbikbaev@gmail.com>
2024-04-15 22:50:43 +0300

    10.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.11.0")
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation("androidx.room:room-rxjava3:$room_version")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
 }

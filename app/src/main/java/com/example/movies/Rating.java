7d9d4865204726abea011103af5dfa0dd80a5451
Tikhon <tihonbikbaev@gmail.com>
2024-04-20 23:43:11 +0300

    10.8

--- app/src/main/java/com/example/movies/Rating.java
+++ app/src/main/java/com/example/movies/Rating.java
@@ -4,13 +4,13 @@ import com.google.gson.annotations.SerializedName;
 
 public class Rating {
     @SerializedName("kp")
-    private String kp;
+    private double kp;
 
-    public String getKp() {
+    public double getKp() {
         return kp;
     }
 
-    public Rating(String kp) {
+    public Rating(double kp) {
         this.kp = kp;
     }
 

ba150cf45fa7f74702c66d9c12944fb664b9110b
Tikhon <tihonbikbaev@gmail.com>
2024-04-20 23:29:31 +0300

    10.7

--- app/src/main/java/com/example/movies/MainActivity.java
+++ app/src/main/java/com/example/movies/MainActivity.java
@@ -1,28 +1,33 @@
 package com.example.movies;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
     private MainViewModel viewModel;
+    private RecyclerView recyclerViewMovies;
+    private MoviesAdapter moviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        moviesAdapter = new MoviesAdapter();
+        recyclerViewMovies.setAdapter(moviesAdapter);
+        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
             @Override
             public void onChanged(List<Movie> movies) {
-                Log.d(TAG, movies.toString());
+                moviesAdapter.setMovies(movies);
             }
         });
         viewModel.loadMovies();
